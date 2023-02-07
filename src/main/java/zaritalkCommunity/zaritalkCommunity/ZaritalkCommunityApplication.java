83a779599eaad07d48ee200db8aa614492995e5e
Sunryeo <elma9700@gmail.com>
2023-02-07 14:32:53 +0900

    프로젝트 생성

---
+++ src/main/java/zaritalkCommunity/zaritalkCommunity/ZaritalkCommunityApplication.java
@@ -0,0 +1,13 @@
+package zaritalkCommunity.zaritalkCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaritalkCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaritalkCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zaritalkCommunity/zaritalkCommunity/ZaritalkCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package zaritalkCommunity.zaritalkCommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaritalkCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
